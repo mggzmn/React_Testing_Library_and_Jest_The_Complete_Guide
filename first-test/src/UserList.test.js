a58c85ec96a886feae78ac2a7661fd568b3849f7
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-16 15:57:25 -0600

    Query function escape hatches, data-testid

---
+++ first-test/src/UserList.test.js
@@ -0,0 +1,22 @@
+import { render, screen, within } from "@testing-library/react";
+import UserList from "./UserList";
+
+test('render one row per user', () => {
+    //Render the component
+    const users = [
+        { name: 'jane', email: 'jane@jane.com' },
+        { name: 'sam', email: 'sam@sam.com' },
+    ]
+    render(<UserList users={users} />)
+    // find all the rows in the table
+    //screen.logTestingPlaygroundURL();
+    const rows = within(screen.getByTestId('users')).getAllByRole('row');
+
+    //assertion: correct number of rows in the table
+    expect(rows).toHaveLength(2);
+});
+test('render the email and name of each user', () => {
+
+});
+
+
