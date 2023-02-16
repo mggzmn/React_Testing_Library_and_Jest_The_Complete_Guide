20914913c0f8f9bd3930b85eb511fd35ce5b6b54
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-16 16:13:52 -0600

    Query selector query function fallback

--- first-test/src/UserList.test.js
+++ first-test/src/UserList.test.js
@@ -15,6 +15,20 @@ test('render one row per user', () => {
     //assertion: correct number of rows in the table
     expect(rows).toHaveLength(2);
 });
+test('render one row per user fallback', () => {
+    //Render the component
+    const users = [
+        { name: 'jane', email: 'jane@jane.com' },
+        { name: 'sam', email: 'sam@sam.com' },
+    ]
+    const { container } = render(<UserList users={users} />)
+    // find all the rows in the table
+    //screen.logTestingPlaygroundURL();
+    const rows = container.querySelectorAll('tbody tr');
+
+    //assertion: correct number of rows in the table
+    expect(rows).toHaveLength(2);
+});
 test('render the email and name of each user', () => {
 
 });
